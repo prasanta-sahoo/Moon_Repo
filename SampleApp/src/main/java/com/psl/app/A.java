2fa4e9ae33fc0defe584f96d49b326bd61306336
Prasanta Sahoo <Prasanta Sahoo>
2016-11-28 00:28:18 +0530

    new project

---
+++ SampleApp/src/main/java/com/psl/app/A.java
@@ -0,0 +1,22 @@
+package com.psl.app;
+
+import org.testng.annotations.Test;
+
+public class A {
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("prasanta");
+	}
+	@Test
+	public void m2()
+	{
+		System.out.println("prasanta1");
+	}
+	@Test
+	public void m3()
+	{
+		System.out.println("prasanta3");
+	}
+}
